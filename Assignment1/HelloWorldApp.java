08260326de79bec4537c298df8c1dc226a657dde
julianlbrainboost <36010110+julianlbrainboost@users.noreply.github.com>
2018-01-31 11:37:08 -0800

    assignment 1

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -7,5 +7,11 @@
 class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Horse");
+        System.out.println("Agumon");
+        System.out.println("1010");
+        System.out.println("He didn't die, that was a clone being telepathically controlled by the real JFK as instructed by the lizard people.");
+        System.out.println("It isn't?");
+        System.out.println("june 21st 2051");
     }
 }
