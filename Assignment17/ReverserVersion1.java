d0f6ef38bca786d153c33245f4c8bc0e697b1f93
julianlbrainboost <36010110+julianlbrainboost@users.noreply.github.com>
2018-05-16 11:23:18 -0700

    assg?//?//?//????

--- Assignment17/ReverserVersion1.java
+++ Assignment17/ReverserVersion1.java
@@ -5,14 +5,17 @@ class ReverserVersion1 {
     public static void main(String[] args) throws IOException {
         int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
         
-        // reverse the data
-        for (int j = 0; j < /*be careful here*/ ; j++) {
-
+        
+        for (int j = 0; j < data.length / 2; j++) {
+            int temp = data[j];
+            //reversed inside data
+            data[j] = data[data.length - j - 1];
+            data[data.length - j -1] = temp;
         }
         
-        // write out the new data
+         
         for (int j = 0; j < data.length; j++) {
-
+            System.out.println(data[j]);
         }
 
     }
